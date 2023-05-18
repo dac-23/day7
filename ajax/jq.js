4e081658b2e1aab4a29f72ce1a3e7bc6892eb357
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-18 12:46:56 +0530

    basic jq. done.

--- ajax/jq.js
+++ ajax/jq.js
@@ -1,4 +1,15 @@
 function makeAjaxCall() {
-  let url = `https://jsonplaceholder.typicode.com/posts/1`;
-  jQuery.get(url);
+  let postTag = document.querySelector("#postid");
+  let url = `https://jsonplaceholder.typicode.com/posts/${postTag.value || 1}`;
+
+  // REQ
+  let jqxhr = jQuery.get(url);
+
+  // RES
+  jqxhr.done((data) => {
+    // DOM
+    let parentTag = document.querySelector("#parent");
+    let newValue = `<h1>${data.body}</h1>`;
+    parentTag.innerHTML = newValue;
+  });
 }
